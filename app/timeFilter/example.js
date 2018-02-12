9bd077f4ebeb7afe419c8631bde2fd8769b19520
aolefirenko <alina.olefirenko@exactuals.com>
2018-02-12 18:20:39 +0200

    init

--- app/timeFilter/example.js
+++ app/timeFilter/example.js
@@ -1,26 +1,9 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', function ($scope) {
+angular.module('angular-font-awesome', ['angular-font-awesome']);
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']).controller('AccordionDemoCtrl', function ($scope) {
+    // var app = angular.module("MesaViewer",[]);
     var ctrl = $scope;
 
     ctrl.oneAtATime = false;
-
-    ctrl.groups = [
-        {
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
-    ctrl.items = ['Item 1', 'Item 2', 'Item 3'];
-
-    ctrl.addItem = function() {
-        var newItemNo = ctrl.items.length + 1;
-        ctrl.items.push('Item ' + newItemNo);
-    };
 
     ctrl.status = {
         mainTimeFilter: false,
@@ -28,7 +11,6 @@ angular.module('ui.bootstrap.demo').controller('AccordionDemoCtrl', function ($s
         halves: false,
         isCustomHeaderOpen: false,
         isFirstOpen: true,
-        isFirstDisabled: false,
-        isOpenTimeFilter: false
+        isFirstDisabled: false
     };
 });
